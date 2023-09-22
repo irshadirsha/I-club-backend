6a57c396f703b5540a1abee83e170606fcf51a11
Mahammad Irshad ap <irshadalike10@gmail.com>
2023-09-22 21:52:13 +0530

    updation

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require ('mongoose')
 require('dotenv').config();
 const connect = ()=>{
-    mongoose.connect('mongodb+srv://irshadalike:irshad111@cluster0.cwnzwvu.mongodb.net/club', {useNewUrlParser: true, useUnifiedTopology: true,}).then(()=>{
+    mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true,}).then(()=>{
         console.log("db connected");
     })
     
